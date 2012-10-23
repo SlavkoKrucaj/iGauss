0695a894f623e9bd1700dba7f1b8de8b7090ac2f
Slavko Krucaj <slavko@infinum.hr>
2012-10-23 17:51:09 +0200

    Initial Commit

---
+++ OutpearTests/OutpearTests.h
@@ -0,0 +1,13 @@
+//
+//  OutpearTests.h
+//  OutpearTests
+//
+//  Created by Slavko Krucaj on 23.10.2012..
+//  Copyright (c) 2012. slavko.krucaj. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OutpearTests : SenTestCase
+
+@end
---
+++ OutpearTests/OutpearTests.m
@@ -0,0 +1,32 @@
+//
+//  OutpearTests.m
+//  OutpearTests
+//
+//  Created by Slavko Krucaj on 23.10.2012..
+//  Copyright (c) 2012. slavko.krucaj. All rights reserved.
+//
+
+#import "OutpearTests.h"
+
+@implementation OutpearTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in OutpearTests");
+}
+
+@end
